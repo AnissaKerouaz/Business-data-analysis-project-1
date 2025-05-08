fa9186092ae70d0c40c2aaac91c8b0b8bbdfd1b5
anissa <anissa.22kerouaz@gmail.com>
2025-05-08 13:28:15 +0100

    project file

---
+++ SQLQuery1.sql
@@ -0,0 +1,88 @@
+--analysing sales performance over time.
+--
+SELECT 
+YEAR(order_date) as order_year,
+MONTH(order_date) as order_month,
+SUM(sales_amount) as total_sales,
+COUNT (DISTINCT customer_key) as total_customers,
+SUM (quantity) as total_quantity
+FROM [gold].[fact_sales]
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date)
+
+-- OR 
+
+SELECT 
+FORMAT(order_date, 'yyyy-MMM') as order_date,
+SUM(sales_amount) as total_sales,
+COUNT (DISTINCT customer_key) as total_customers,
+SUM (quantity) as total_quantity
+FROM [gold].[fact_sales]
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')-- Groups results by year and month
+ORDER BY FORMAT(order_date, 'yyyy-MMM')-- Sorts the results chronologically
+
+--calculate the total sales per month
+--calculate the running total of sales over time
+SELECT 
+order_date,
+total_sales,
+SUM (total_sales) OVER (PARTITION BY order_date ORDER BY order_date) AS running_total_sales--OVER tells SQL:“Apply this function across a set of rows, not just one row — and do it in a specific order or partition.”
+--For each row, calculate the sum of total_sales **from the beginning
+FROM 
+(
+SELECT 
+CAST(DATEFROMPARTS(YEAR(order_date), MONTH(order_date), 1) AS DATE) AS order_date,
+SUM(sales_amount) as total_sales
+FROM [gold].[fact_sales]
+where order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+) AS tableu --Run this subquery first, and call its result t so I can use it like a table.
+--by the year
+SELECT 
+order_date,
+total_sales,
+SUM (total_sales) OVER ( ORDER BY order_date) AS running_total_sales--OVER tells SQL:“Apply this function across a set of rows, not just one row — and do it in a specific order or partition.”
+--For each row, calculate the sum of total_sales **from the beginning
+FROM 
+(
+SELECT 
+CAST(DATEFROMPARTS(YEAR(order_date), 1, 1) AS DATE) AS order_date,
+SUM(sales_amount) as total_sales
+FROM [gold].[fact_sales]
+where order_date IS NOT NULL
+GROUP BY YEAR(order_date) -- ,MONTH(order_date)
+) AS tableu
+
+--analyse the yearly performance of products by comparing each product's sales to both 
+--its average sales performance and the previous year's performance
+WITH yearly_product_sales AS (
+SELECT 
+YEAR(f.order_date) AS order_year,
+d.product_name,
+SUM(f.sales_amount) AS current_sales
+FROM [gold].[fact_sales] f
+LEFT JOIN [gold].[dim_products] d
+ON f.product_key = d.product_key
+GROUP BY YEAR(f.order_date), d.product_name)
+SELECT 
+order_year,
+product_name,
+current_sales,
+AVG(current_sales) OVER (PARTITION BY product_name) AS avg_sales,
+current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg,
+CASE WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'above avg'
+     WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'below avg'
+	 ELSE 'avg'
+END avg_change,
+LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS porductyear_sales,
+current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS diff_productyear,
+CASE WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0 THEN 'inc'
+     WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) < 0 THEN 'dec'
+	 ELSE 'no change'
+END productyear_change
+FROM yearly_product_sales
+ORDER BY product_name, order_year
+
+
