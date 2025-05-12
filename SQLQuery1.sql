a04f767932b5cea1347f4ba93e9dc8cdf8277dbe
anissa <anissa.22kerouaz@gmail.com>
2025-05-12 13:53:17 +0100

    data segmentation

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -103,3 +103,60 @@ CONCAT(ROUND ((CAST (total_sales AS FLOAT) / SUM (total_sales) OVER ()) * 100,2)
 FROM category_sales
 order by total_sales DESC
 
+--data segmentation
+--segmenting products into cost ranges and count how many products fall into each segment.
+
+
+WITH product_segments AS (
+SELECT
+product_key,
+product_name,
+cost,
+CASE WHEN cost < 100 THEN 'below 100'
+     WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+	 WHEN cost BETWEEN 500 AND 1000 THEN  '500-1000'
+	 ELSE 'Above 1000'
+END cost_range
+FROM gold.dim_products)
+
+SELECT 
+cost_range,
+COUNT(product_key) AS total_products
+FROM product_segments
+GROUP BY cost_range
+ORDER BY total_products DESC
+
+--GROUPING CUSTOMERS INTO THREE SEGMENTS BASED ON THEIR SPENDING BEHAVIOR:
+--_VIP : at leat 12 months of history and spending more than $5000.
+--_regular : at least 12 months of history but spending $5000 or less.
+--_new : lifespan less than 12 months.
+WITH customer_spending AS (
+SELECT 
+c.customer_key,
+SUM(f.sales_amount) AS total_spending,
+MIN(order_date) AS first_order,
+MAX(order_date) AS last_order,
+DATEDIFF (month, MIN(order_date), MAX(order_date)) AS lifespan
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON f.customer_key = c.customer_key
+GROUP BY c.customer_key
+)
+
+SELECT 
+customer_segment,
+COUNT(customer_key) AS total_customers
+FROM(
+
+SELECT 
+customer_key,
+CASE WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+     WHEN LIFESPAN >= 12 AND total_spending <= 5000 THEN 'regular'
+	 ELSE 'NEW'
+END customer_segment
+FROM customer_spending) t
+GROUP BY customer_segment 
+ORDER BY total_customers DESC
+
+
+
